5cdae2470f31a060b935329e4603f7cafeffe8ce
Lucas Iron <lucas.iron@hotmail.com.br>
2017-06-10 21:38:27 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', function () {
+	'use strict';
+	var http = new XMLHttpRequest();
+	http.overrideMimeType('application/json');
+	http.open('GET', '/assets/potions.json', true);
+	http.onreadystatechange = function () {
+		if (http.readyState === 4 && http.status === '200') {
+			var products = JSON.parse(http.responseText);
+			var ul = document.getElementById('product-ul');
+			products.potions.forEach(function (potion) {
+				var li = document.createElement('li');
+				var img = document.createElement('img');
+				img.src = '/assets/products/' + potion.image;
+				li.appendChild(img);
+				var p = document.createElement('p');
+				var node = document.createTextNode(potion.name + ' - ');
+				p.appendChild(node);
+				var span = document.createElement('span');
+				node = document.createTextNode(potion.price);
+				span.appendChild(node);
+				p.appendChild(span);
+				li.appendChild(p);
+				ul.appendChild(li);
+			});
+		}
+	}
+	http.send(null);
+}, false);
